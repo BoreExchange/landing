0a6fdad02daeb1ef2baa46b776c643538a7a2edd
charlesgpt <admin@jamzify.com>
2023-11-23 19:51:16 +0400

    2

--- src/data/collection-data.js
+++ src/data/collection-data.js
@@ -2,49 +2,49 @@ const collection_data = [
   {
     id:1,
     img:'/assets/img/artwork/0011.jpg',
-    name:'Orientation - Africa',
+    name:'',
     author:'Bore'
   },
   {
     id:2,
     img:'/assets/img/artwork/0022.jpg',
-    name:'Orientation - Africa',
+    name:'',
     author:'Bore'
   },
   {
     id:3,
     img:'/assets/img/artwork/0032.jpg',
-    name:'Women empowerment',
+    name:'',
     author:'Bore'
   },
   {
     id:4,
     img:'/assets/img/artwork/0044.jpg',
-    name:'Sensitization',
+    name:'',
     author:'Bore'
   },
   {
     id:5,
     img:'/assets/img/artwork/0055.jpg',
-    name:'Charity',
+    name:'',
     author:'Bore'
   },
   {
     id:6,
     img:'/assets/img/artwork/0066.jpg',
-    name:'Charity',
+    name:'',
     author:'Bore'
   },
   {
     id:7,
     img:'/assets/img/artwork/0077.jpg',
-    name:'Charity',
+    name:'',
     author:'Bore'
   },
   {
     id:8,
     img:'/assets/img/artwork/0088.jpg',
-    name:'Charity',
+    name:'',
     author:'Bore'
   },
 ]
