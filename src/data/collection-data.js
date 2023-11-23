ed3d9874da71122c53f0d7f91d528c7dbac0ea41
charlesgpt <admin@jamzify.com>
2023-11-23 19:50:41 +0400

    21

--- src/data/collection-data.js
+++ src/data/collection-data.js
@@ -1,49 +1,49 @@
 const collection_data = [
   {
     id:1,
-    img:'/assets/img/artwork/001.jpg',
+    img:'/assets/img/artwork/0011.jpg',
     name:'Orientation - Africa',
     author:'Bore'
   },
   {
     id:2,
-    img:'/assets/img/artwork/002.jpg',
+    img:'/assets/img/artwork/0022.jpg',
     name:'Orientation - Africa',
     author:'Bore'
   },
   {
     id:3,
-    img:'/assets/img/artwork/003.jpg',
+    img:'/assets/img/artwork/0032.jpg',
     name:'Women empowerment',
     author:'Bore'
   },
   {
     id:4,
-    img:'/assets/img/artwork/004.jpg',
+    img:'/assets/img/artwork/0044.jpg',
     name:'Sensitization',
     author:'Bore'
   },
   {
     id:5,
-    img:'/assets/img/artwork/005.jpg',
+    img:'/assets/img/artwork/0055.jpg',
     name:'Charity',
     author:'Bore'
   },
   {
     id:6,
-    img:'/assets/img/artwork/006.jpg',
+    img:'/assets/img/artwork/0066.jpg',
     name:'Charity',
     author:'Bore'
   },
   {
     id:7,
-    img:'/assets/img/artwork/007.jpg',
+    img:'/assets/img/artwork/0077.jpg',
     name:'Charity',
     author:'Bore'
   },
   {
     id:8,
-    img:'/assets/img/artwork/008.jpg',
+    img:'/assets/img/artwork/0088.jpg',
     name:'Charity',
     author:'Bore'
   },
